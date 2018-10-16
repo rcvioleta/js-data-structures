b4d25051d92a6ae541b0be16b18814d6267023da
Roge <rcvioleta13@gmail.com>
2018-10-16 16:44:05 +0800

    first commit

---
+++ Binary Search Tree/main.js
@@ -0,0 +1,57 @@
+function BST(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+}
+
+BST.prototype.insert = function(value) {
+    // insert value to the left
+    if (value <= this.value) {
+        !this.left ? this.left = new BST(value) : this.left.insert(value);
+    }
+    // insert value to the right
+    else if (value > this.value) {
+        !this.right ? this.right = new BST(value) : this.right.insert(value);
+    }
+} 
+
+BST.prototype.contains = function(value) {
+    if (value === this.value) return true;
+    else if (value < this.value) {
+        if (!this.left) return false;
+        else return this.left.contains(value);
+    } 
+    else if (value > this.value) {
+        if (!this.right) return false;
+        else return this.right.contains(value);
+    }
+}
+
+BST.prototype.depthFirstTraversal = function(iteratorFunc, order) {
+    if (order === 'pre-order') iteratorFunc(this.value);
+    if (this.left) this.left.depthFirstTraversal(iteratorFunc, order);
+    if (order === 'in-order') iteratorFunc(this.value);
+    if (this.right) this.right.depthFirstTraversal(iteratorFunc, order);
+    if (order === 'post-order') iteratorFunc(this.value);
+}
+
+function log(value) {
+    console.log(value);
+}
+
+let bst = new BST(50);
+
+bst.insert(30);
+bst.insert(70);
+bst.insert(100);
+bst.insert(60);
+bst.insert(59);
+bst.insert(20);
+bst.insert(45);
+bst.insert(35);
+bst.insert(85);
+bst.insert(105);
+bst.insert(10);
+
+// console.log(bst);
+bst.depthFirstTraversal(log, 'post-order');
